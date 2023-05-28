7267ef7fa78842a10bd2737ef3d28da20def8b5f
Yao Wang <dell9459@gmail.com>
2023-05-28 16:49:41 -0400

    added annotation for employee repository

--- src/main/java/com/luv2code/springboot/cruddemo/repository/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/cruddemo/repository/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.luv2code.springboot.cruddemo.repository;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
